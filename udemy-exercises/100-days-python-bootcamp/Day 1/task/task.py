1cd030ec0616510143a223982ad52a0c3f68177b
VenkateshP-coder <venkateshpasala.ui@gmail.com>
2026-06-25 17:37:06 -0400

    Added JS files

--- udemy-exercises/100-days-python-bootcamp/Day 1/task/task.py
+++ udemy-exercises/100-days-python-bootcamp/Day 1/task/task.py
@@ -1,9 +1,15 @@
-print("Hello World!!")
-print("Hello World!!\nHello World!!")
-
-print("Hello" + " World" + "!!")
-print("Hello "+"World!!")
-print(len(input("what's your name\n")))
-name = input("What's your name")
-length = len(name)
-print(length)
+# print("Hello World!!")
+# print("Hello World!!\nHello World!!")
+#
+# print("Hello" + " World" + "!!")
+# print("Hello "+"World!!")
+# print(len(input("what's your name\n")))
+# name = input("What's your name")
+# length = len(name)
+# print(length)
+#this is using input() function
+print("Hello " + input("What is your name?") + "!")
+# Variables
+print(len(input("What's your name?")))
+name = input("Whats your name")
+print(len(name))
